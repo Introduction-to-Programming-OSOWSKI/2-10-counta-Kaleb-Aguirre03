bd7ada3dcbe5941870d9a32e886bbad922079629
Kaleb-Aguirre03 <820930@mystma.org>
2021-10-06 09:29:54 -0500

    #1

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-#WRITE YOUR CODE IN THIS FILE
+#Define the function countA() in order to count the number of "a's" in a string
+def countA(w):
+
+    w = w.lower()
+
+    numberA = 0
+
+    for i in range (0, len(w)):
+        if w[i] == "a":
+            numberA = numberA + 1
+
+    return numberA
+
+#print the function in order to test it
+print (countA("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
